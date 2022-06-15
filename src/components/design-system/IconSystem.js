f8fbd069dc7b25b419e2e72ffcc7d53fa6a3b1e1
two-vs-han <daehan1028@naver.com>
2022-06-15 15:40:46 +0900

    CHORE: move leaf icon to icon system

--- src/components/design-system/IconSystem.js
+++ src/components/design-system/IconSystem.js
@@ -45,3 +45,29 @@ export const ChevronLeftIcon = () => {
     </Svg>
   );
 };
+
+export const LeafIcon = () => {
+  return (
+    <Svg
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <Path
+        d="M5 21C5.5 16.5 7.5 13 12 11"
+        stroke="#fff"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <Path
+        d="M8.99999 18C15.218 18 19.5 14.712 20 6V4H15.986C6.98599 4 3.99999 8 3.98599 13C3.98599 14 3.98599 16 5.98599 18H8.98599H8.99999Z"
+        stroke="#fff"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
+  );
+};
